0aefd46b7c3634ad129f6546ae399368cf4db3ac
宋格 <1253032175@qq.com>
2018-11-13 11:17:44 +0800

    1

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var wechat_file = path.join(__dirname,'/config/wechat.txt')
+var wechat_sdk = path.join(__dirname,'/config/sdk.txt')
 var util = require('./libs/util')
 var config = {
   wechat:{
@@ -12,7 +13,14 @@ var config = {
     saveAccessToken :function(data){
       data = JSON.stringify(data) 
       return util.writeFileAsync(wechat_file,data)
-    }
+    },
+    getTicket :function(){
+      return util.readFileAsync(wechat_sdk)
+    },
+    saveTicket :function(data){
+      data = JSON.stringify(data) 
+      return util.writeFileAsync(wechat_sdk,data)
+    },
   }
 }
 module.exports = config
