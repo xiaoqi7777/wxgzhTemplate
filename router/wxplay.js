adff9f3e37eb5f061408f5324423a1804b2875db
宋格 <1253032175@qq.com>
2018-11-14 18:33:20 +0800

    1

--- router/wxplay.js
+++ router/wxplay.js
@@ -22,7 +22,7 @@ let body = '商品名称'
 let total_fee = '1'
 let detail = '商品详情'
 let trade_type = 'NATIVE'
-let product_id = '007'
+let product_id = nonce_str
 let out_trade_no = moment().local().format('YYYYMMDDhhmmss') //商户订单号
 let timeStamp = moment().unix().toString() //时间戳
 
@@ -103,10 +103,11 @@ router.get('/getOpenId',async(x,next) => {
 
 router.post('/commonPay',async(x, next) => {
   let data = x.request.body
-  order.total_fee = data.money
-  order.trade_type = 'JSAPI'
-
-
+  total_fee = order.total_fee = data.money
+  trade_type = order.trade_type = 'JSAPI'
+  product_id = order.product_id = randomstring.generate(32)
+  out_trade_no = order.out_trade_no = moment().local().format('YYYYMMDDhhmmss') 
+  body = order.body = '暂时=>'+randomstring.generate(5)
   sign =  wxSign(order, key)
 
   let xmlOrder = xmljs.js2xml({
@@ -139,7 +140,7 @@ console.log('调取支付微信返回的结果',prepay)
     timeStamp:timeStamp,
     nonceStr:nonce_str,
     package:`prepay_id=${prepay_id}`,
-    signType : 'md5'
+    signType : 'MD5'
   }
   sign = wxSign(params,key)
   console.log('发给前端的签名',sign)
@@ -149,7 +150,7 @@ console.log('调取支付微信返回的结果',prepay)
     timeStamp: timeStamp, //时间戳，自1970年以来的秒数
     nonceStr: nonce_str, //随机串
     package: prepay_id,
-    signType: "md5", //微信签名方式：
+    signType: "MD5", //微信签名方式：
     paySign: sign //微信签名
   }
   x.body = obj
@@ -158,7 +159,11 @@ console.log('调取支付微信返回的结果',prepay)
 
 router.post('/wx', async (x, next) => {
   let data = x.request.body
-  order.total_fee = data.data
+  total_fee = order.total_fee = data.data
+  trade_type = order.trade_type = 'NATIVE'
+  product_id = order.product_id = randomstring.generate(32)
+  out_trade_no = order.out_trade_no = moment().local().format('YYYYMMDDhhmmss') 
+  body = order.body = '暂时=>'+randomstring.generate(5)
   //获取签名
   sign =  wxSign(order, key)
   //转换成 xml 格式
